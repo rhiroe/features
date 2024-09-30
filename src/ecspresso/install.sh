b09ae977c40dc6b8d6db698d76202c135b7b626f
rhiroe <ride.poke@gmail.com>
2024-09-30 16:40:12 +0000

    Consider the architecture

--- src/ecspresso/install.sh
+++ src/ecspresso/install.sh
@@ -10,15 +10,25 @@ set -a
 . ./devcontainer-features.env
 set +a
 
+architecture="$(uname -m)"
+case ${architecture} in
+    x86_64) architecture="amd64";;
+    aarch64 | armv8*) architecture="arm64";;
+    aarch32 | armv7* | armvhf*) architecture="arm";;
+    i?86) architecture="386";;
+    *) echo "(!) Architecture ${architecture} unsupported"; exit 1 ;;
+esac
+
 if [ "${ECSPRESSOVERSION}" = "latest" ]; then
     ECSPRESSO_VERSION="2.4.1"
 else
     ECSPRESSO_VERSION="${ECSPRESSOVERSION:-"2.4.1"}"
 fi
-ECSPRESSO_DOWNLOAD_URL=https://github.com/kayac/ecspresso/releases/download/v${ECSPRESSO_VERSION}/ecspresso_${ECSPRESSO_VERSION}_linux_amd64.tar.gz
+ECSPRESSO_DOWNLOAD_URL=https://github.com/kayac/ecspresso/releases/download/v${ECSPRESSO_VERSION}/ecspresso_${ECSPRESSO_VERSION}_linux_${architecture}.tar.gz
 JQ_DOWNLOAD_URL=https://github.com/stedolan/jq/releases/latest/download/jq-linux64
-YQ_DOWNLOAD_URL=https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
-SESSION_MANAGER_PLUGIN_DOWNLOAD_URL=https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_arm64/session-manager-plugin.deb
+YQ_DOWNLOAD_URL=https://github.com/mikefarah/yq/releases/latest/download/yq_linux_${architecture}
+if [ "${architecture}" = "arm64" ]; then SM_ARCH=ubuntu_arm64; else SM_ARCH=ubuntu_64bit; fi
+SESSION_MANAGER_PLUGIN_DOWNLOAD_URL=https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_${SM_ARCH}/session-manager-plugin.deb
 
 apt update && \
 apt install -y ca-certificates wget && \
